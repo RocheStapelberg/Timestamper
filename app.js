9b6db4c512176b15a490d167861b6435e4d0b8b3
Roche Stapelberg <stapelberg.roche@gmail.com>
2022-06-02 15:02:19 +0200

    Finished main page and routing to main page

---
+++ app.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let app = express();
+
+// Setup for env and assets
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST;
+
+assetPath = __dirname + "/Public";
+
+app.use("/public", express.static(assetPath));
+app.use(express.json());
+
+// Serving front-page
+app.get('/', (req, res) => {
+    let now = new Date();
+    console.log('\x1b[36m%s\x1b[0m', `Main page was requested | Time: ${now} `);
+    res.sendFile(__dirname + '/views/index.html')
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`Listening on http://${HOST}:${PORT}`);
+});
