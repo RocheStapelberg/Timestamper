b9549427f5de4d166b3fac40d67bc9f5ce17a5b5
Roche Stapelberg <stapelberg.roche@gmail.com>
2022-06-03 19:06:57 +0200

    Added comments

--- routes/timestamper.js
+++ routes/timestamper.js
@@ -1,12 +1,15 @@
 let express = require('express');
 const timeStampRouter = express.Router();
 
+// Stamper route that takes in a unix date or yyyy-mm-dd
 timeStampRouter.get('/:time', (req, res) => {
+    // Fetches time from url.
     let time = req.params.time;
     let data = {};
     let unixTime;
     let result;
 
+    // Check if time is in unix format which is an integer.
     if (isInteger(time)) {
         unixTime = Number(time);
         result = new Date(time / 1).toUTCString();
@@ -15,8 +18,10 @@ timeStampRouter.get('/:time', (req, res) => {
         unixTime = Math.floor(new Date(time) / 1);
     }
 
+    // Creates JSON object from data.
     data = {"unix": unixTime, "utc": result};
     
+    // Simple logging
     let now = new Date();
     console.log('\x1b[36m%s\x1b[0m', `New request for date ${ time } | Time: ${ now } `);
     res.json(data)
