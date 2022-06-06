d05d40f2b6b1f314fe6d23f9062c176d0e2aef7c
Roche Stapelberg <stapelberg.roche@gmail.com>
2022-06-06 15:39:39 +0200

    Added validation checks and new route

--- routes/timestamper.js
+++ routes/timestamper.js
@@ -1,6 +1,18 @@
 let express = require('express');
 const timeStampRouter = express.Router();
 
+// Return current date if no parameter was given
+timeStampRouter.get('/', (req, res) => {
+    const utc = new Date().toUTCString();
+    const unixTime = Math.floor(new Date(utc) / 1);
+
+    const data = {"unix": unixTime, "utc": utc}
+
+    let now = new Date();
+    console.log('\x1b[36m%s\x1b[0m', `New request for date now | Time: ${ now } `);
+    res.json(data);
+});
+
 // Stamper route that takes in a unix date or yyyy-mm-dd
 timeStampRouter.get('/:time', (req, res) => {
     // Fetches time from url.
@@ -9,17 +21,18 @@ timeStampRouter.get('/:time', (req, res) => {
     let unixTime;
     let result;
 
-    // Check if time is in unix format which is an integer.
-    if (isInteger(time)) {
+    // Validation checks
+    if(isInteger(time)){
         unixTime = Number(time);
         result = new Date(time / 1).toUTCString();
-    } else {
+        data = {"unix": unixTime, "utc": result};
+    } else if (isDate(time)) {
         result = new Date(time).toUTCString();
         unixTime = Math.floor(new Date(time) / 1);
+        data = {"unix": unixTime, "utc": result};
+    } else {
+        data = {"error" : "Invalid Date"}
     }
-
-    // Creates JSON object from data.
-    data = {"unix": unixTime, "utc": result};
     
     // Simple logging
     let now = new Date();
@@ -30,7 +43,12 @@ timeStampRouter.get('/:time', (req, res) => {
 // Function to check if value is a integer.
 function isInteger(value) {
     return /^\d+$/.test(value);
-    }
+}
+
+// Function to check if value is a Date.
+function isDate(date) {
+    return (new Date(date) !== "Invalid Date") && !isNaN(new Date(date));
+}
 
 module.exports = timeStampRouter;
 
